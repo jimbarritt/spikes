bbac4c56190ea47e25188dcc3e0a7361e2465ae4
Jim Barritt <jim@planet-ix.com>
2010-04-06 21:45:45 +0100

    Added some basic tests of matching multiple groups

---
+++ regex/src/test/unit/java/com/jimbarritt/spikes/regex/FindMultipleMatchesTest.java
@@ -0,0 +1,33 @@
+package com.jimbarritt.spikes.regex;
+
+import org.junit.*;
+import org.apache.log4j.*;
+
+import java.util.regex.*;
+
+public class FindMultipleMatchesTest {
+
+    private static final Logger log = Logger.getLogger(FindMultipleMatchesTest.class);
+
+    @Test
+    public void findsMultipleGroups() {
+        Pattern pattern = Pattern.compile("\\w* ");
+
+        Matcher matcher = pattern.matcher("this is a sentance");
+        log.info("GroupCount: " + matcher.groupCount());
+        while (matcher.find()) {
+            log.info("Found group : [" + matcher.group().trim() + "]");
+        }
+    }
+
+    @Test
+    public void findStructuredGroups() {
+        Pattern pattern = Pattern.compile("\\{\\w*\\}");
+        Matcher matcher = pattern.matcher("/{parameter1}/some/path/{parameter2}/end");
+        while(matcher.find()) {
+            log.info("Found parameter: [" + matcher.group().replaceAll("\\{|\\}", "") + "]");
+        }
+        String output = matcher.replaceAll("(.*)");
+        log.info("Replaced output: [" + output + "]");
+    }
+}

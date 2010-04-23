77ddb349d025e44cf0934dbca2ca0400781009c3
Jim Barritt <jim@planet-ix.com>
2010-04-23 18:40:15 +0100

    added test for url matcher

--- regex/src/test/unit/java/com/jimbarritt/spikes/regex/MatchUpToTest.java
+++ regex/src/test/unit/java/com/jimbarritt/spikes/regex/MatchUpToTest.java
@@ -1,7 +1,7 @@
 package com.jimbarritt.spikes.regex;
 
-import org.junit.*;
 import org.apache.log4j.*;
+import org.junit.*;
 
 import java.util.regex.*;
 
@@ -16,10 +16,41 @@ public class MatchUpToTest {
 
         Matcher matcher = pattern.matcher("some/path/to/file.css");
 
+        printMatcher(matcher);
+    }
+
+    @Test
+    public void optionallyMatchAGroup() {
+        String regex = "/([^.]*)(\\..*)?";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher("/some/path/to/file");
+        printMatcher(matcher);
+
+        matcher = pattern.matcher("/some/path/to/file.css");
+        printMatcher(matcher);
+    }
+
+    @Test
+    public void matchesOptionalStuff() {
+        String regex = "(<hr)( +SIZE *= *[0-9]+)? *";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher("<hr SIZE=23 ");
+        printMatcher(matcher);
+
+        matcher = pattern.matcher("<hr ");
+        printMatcher(matcher);
+
+    }
+
+    public static void printMatcher(Matcher matcher) {
         boolean matches = matcher.matches();
         log.info("Matches      : " + matches);
         log.info("Group Count  : " + matcher.groupCount());
-        for (int i=0; i<=matcher.groupCount(); i++) {
+        for (int i = 0; i <= matcher.groupCount(); i++) {
             log.info("Group [" + i + "]  :" + matcher.group(i));
         }
     }

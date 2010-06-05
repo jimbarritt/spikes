f340e1ea904a69d235dd82fbcac61b682e800c46
Jim Barritt <jim@planet-ix.com>
2010-06-05 15:26:01 +0100

    Trying to get unit tests to work

--- ruby-demo/spec/RubyDemo/hello_test.rb
+++ ruby-demo/spec/RubyDemo/hello_test.rb
@@ -1,4 +1,12 @@
-require 'RubyDemo/hello.rb'
+require "test/unit"
+require "RubyDemo/hello"
 
+class TC_HelloTest < Test::Unit::TestCase
+
+  def test_fail
+    hello = RubyDemo::Hello.new
+    assert(hello.speak, true)
+  end
+end
 
 

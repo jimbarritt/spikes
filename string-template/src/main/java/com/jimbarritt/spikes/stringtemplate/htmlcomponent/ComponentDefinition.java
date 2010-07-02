089158566cadb2482975e8681a52387377113167
Jim Barritt <jim@planet-ix.com>
2010-07-02 22:27:13 +0100

    Added custom map

--- string-template/src/main/java/com/jimbarritt/spikes/stringtemplate/htmlcomponent/ComponentDefinition.java
+++ string-template/src/main/java/com/jimbarritt/spikes/stringtemplate/htmlcomponent/ComponentDefinition.java
@@ -2,12 +2,16 @@ package com.jimbarritt.spikes.stringtemplate.htmlcomponent;
 
 import org.antlr.stringtemplate.*;
 
-public class ComponentDefinition {
+import java.util.*;
+
+public class ComponentDefinition extends HashMap<String, String> {
+
+    public ComponentDefinition() {
+        super.put("textComponent", "<div>This is my text component</div>");
+    }
+
     public void configureTemplate(StringTemplate stringTemplate) {
         stringTemplate.setAttribute("define", this);
     }
-
-    public String getComponent() {
-        return "Component";
-    }
+    
 }

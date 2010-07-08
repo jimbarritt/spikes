cacb20d9c6d4192941f536a07e1aed1390dc7eea
Jim Barritt <jim@planet-ix.com>
2010-07-08 17:19:49 +0100

    Added simple webdrive test

--- java-2d/src/test/java/com/jimbarritt/spikes/java2d/imagecompare/CaptureImagesTest.java
+++ java-2d/src/test/java/com/jimbarritt/spikes/java2d/imagecompare/CaptureImagesTest.java
@@ -1,13 +1,23 @@
 package com.jimbarritt.spikes.java2d.imagecompare;
 
+import org.apache.log4j.*;
 import org.junit.*;
 import org.openqa.selenium.*;
 
+import java.net.*;
+
+import static java.lang.Thread.currentThread;
+
 public class CaptureImagesTest {
+    private static final Logger log = Logger.getLogger(CaptureImagesTest.class);
 
     @Test
     public void createsImagesFromHtml() throws Exception {
         WebDriver webDriver = new WebDriverFactory().useFirefox().createWebDriver();
-        
+
+        URL masterUrl = currentThread().getContextClassLoader().getResource("html/testPage_master.xhtml");
+        webDriver.get(masterUrl.toExternalForm());
+
+        log.info("SOURCE:\n[[" + webDriver.getPageSource() + "]]");
     }
 }

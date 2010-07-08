cc90cbcd2e7583c2e6ed49e6abb961ce226de8f6
Jim Barritt <jim@planet-ix.com>
2010-07-08 17:14:17 +0100

    Adding webdriver support jars

---
+++ java-2d/src/test/java/com/jimbarritt/spikes/java2d/imagecompare/CaptureImagesTest.java
@@ -0,0 +1,13 @@
+package com.jimbarritt.spikes.java2d.imagecompare;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+
+public class CaptureImagesTest {
+
+    @Test
+    public void createsImagesFromHtml() throws Exception {
+        WebDriver webDriver = new WebDriverFactory().useFirefox().createWebDriver();
+        
+    }
+}
---
+++ java-2d/src/test/java/com/jimbarritt/spikes/java2d/imagecompare/WebDriverFactory.java
@@ -0,0 +1,35 @@
+package com.jimbarritt.spikes.java2d.imagecompare;
+
+import org.apache.log4j.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.htmlunit.*;
+
+public class WebDriverFactory {
+    private static final Logger log = Logger.getLogger(WebDriverFactory.class);
+
+    private static final WebDriverFactory FIREFOX_FACTORY = new WebDriverFactory() {
+        @Override public WebDriver createWebDriver() {
+            return new FirefoxDriver();
+        }
+    };
+
+    private static final WebDriverFactory HTMLUNIT_FACTORY = new WebDriverFactory() {
+        @Override public WebDriver createWebDriver() {
+            return new HtmlUnitDriver();
+        }
+    };
+
+    private WebDriverFactory factory = HTMLUNIT_FACTORY;
+
+    public WebDriver createWebDriver() {
+        WebDriver webDriver = factory.createWebDriver();
+        log.debug("Instantiating WebDriver of type: " + webDriver.getClass().getSimpleName());
+        return webDriver;
+    }
+
+    public WebDriverFactory useFirefox() {
+        this.factory = FIREFOX_FACTORY;
+        return this;
+    }
+}

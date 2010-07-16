edb254e49f3116fc12def7fbed8b01fa4c03243a
Jim Barritt <jim@planet-ix.com>
2010-07-16 13:22:52 +0100

    Committing after being away from code

--- string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/io/InterfaceStringTemplateGroupFactoryTest.java
+++ string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/io/InterfaceStringTemplateGroupFactoryTest.java
@@ -1,9 +1,33 @@
 package com.jimbarritt.spikes.stringtemplate.io;
 
+import org.antlr.stringtemplate.*;
 import org.junit.*;
+
+import java.io.*;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class InterfaceStringTemplateGroupFactoryTest {
 
     @Test
-    public void create
+    public void createsInterfaceBasedGroup() {
+        InterfaceStringTemplateGroupFactory factory = new InterfaceStringTemplateGroupFactory();
+
+        StringTemplateGroup group = factory.createGroupFromRootPath("testGroup", StringTemplateRootPath.getStringTemplateRootDir(), StringTemplateGroup.DEFAULT_ERROR_LISTENER);
+
+        assertThat(group, is(instanceOf(InterfaceBasedStringTemplateGroup.class)));
+    }
+
+    @Test
+    public void createsInterfaceBasedGroupFromReader() {
+        String groupContents = "group someGroup;";
+
+        InterfaceStringTemplateGroupFactory factory = new InterfaceStringTemplateGroupFactory();
+
+        StringTemplateGroup group = factory.createGroupFromReader(new StringReader(groupContents), StringTemplateRootPath.getStringTemplateRootDir(), StringTemplateGroup.DEFAULT_ERROR_LISTENER);
+
+        assertThat(group, is(instanceOf(InterfaceBasedStringTemplateGroup.class)));
+    }
 }

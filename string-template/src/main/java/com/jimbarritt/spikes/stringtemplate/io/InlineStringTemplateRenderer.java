a0428f58474f9199c5ef3678ddadb4d9b38cd8f6
Jim Barritt <jim@planet-ix.com>
2010-07-23 10:11:57 +0100

    Added with().attribute()

--- string-template/src/main/java/com/jimbarritt/spikes/stringtemplate/io/InlineStringTemplateRenderer.java
+++ string-template/src/main/java/com/jimbarritt/spikes/stringtemplate/io/InlineStringTemplateRenderer.java
@@ -6,25 +6,37 @@ import org.antlr.stringtemplate.*;
 import java.util.*;
 
 public class InlineStringTemplateRenderer {
-    public static String renderStringTemplate(String template, StringTemplateAttribute... attributes) {
+    public static String renderStringTemplate(String template, AttributeList attributes) {
         StringTemplate stringTemplate = new StringTemplate(template);
         stringTemplate.registerRenderer(Volume.class, new VolumeAttributeRenderer());
-        for (StringTemplateAttribute attribute : attributes) {
+        for (Attribute attribute : attributes) {
             attribute.registerIn(stringTemplate);
         }
         return stringTemplate.toString();
     }
 
-    public static StringTemplateAttribute attribute(String key, Object value) {
-        return new StringTemplateAttribute(key, value);
+    public static AttributeList with() {
+        return new AttributeList();
     }
-    
 
-    public static class StringTemplateAttribute {
+    public static class AttributeList implements Iterable<Attribute> {
+        private List<Attribute> attributes = new ArrayList<Attribute>();
+
+        public AttributeList attribute(String key, Object value) {
+            attributes.add(new Attribute(key, value));
+            return this;
+        }
+
+        @Override public Iterator<Attribute> iterator() {
+            return attributes.iterator();
+        }
+    }
+
+    public static class Attribute {
         private String key;
         private Object value;
 
-        public StringTemplateAttribute(String key, Object value) {
+        public Attribute(String key, Object value) {
             this.key = key;
             this.value = value;
         }
--- string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/volume/VolumeAttributeRendererTest.java
+++ string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/volume/VolumeAttributeRendererTest.java
@@ -2,18 +2,19 @@ package com.jimbarritt.spikes.stringtemplate.volume;
 
 import org.junit.*;
 
-import static com.jimbarritt.spikes.stringtemplate.io.InlineStringTemplateRenderer.attribute;
-import static com.jimbarritt.spikes.stringtemplate.io.InlineStringTemplateRenderer.renderStringTemplate;
+import static com.jimbarritt.spikes.stringtemplate.io.InlineStringTemplateRenderer.*;
 import static com.jimbarritt.spikes.stringtemplate.volume.Volume.UnitOfMeasure.*;
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
 public class VolumeAttributeRendererTest {
 
+    
+
     @Test
     public void formatsInLitres() {
         String representation = renderStringTemplate("My car has an engine size of $engineSize;format=\"litres\"$",
-                                                     attribute("engineSize", new Volume(20, LITRES)));
+                                                     with().attribute("engineSize", new Volume(20, LITRES)));
 
         assertThat(representation, is("My car has an engine size of 20.00 litres"));
     }
@@ -21,7 +22,7 @@ public class VolumeAttributeRendererTest {
     @Test
     public void formatsInLitresWithShortFormat() {
         String representation = renderStringTemplate("My car has an engine size of $engineSize;format=\"litres-short\"$",
-                                                attribute("engineSize", new Volume(20, LITRES)));
+                                                     with().attribute("engineSize", new Volume(20, LITRES)));
 
         assertThat(representation, is("My car has an engine size of 20.00l"));
     }
@@ -30,7 +31,7 @@ public class VolumeAttributeRendererTest {
     @Test
     public void convertsToCc() {
         String representation = renderStringTemplate("My car has an engine size of $engineSize;format=\"cc\"$",
-                                                        attribute("engineSize", new Volume(20, LITRES)));
+                                                     with().attribute("engineSize", new Volume(20, LITRES)));
 
         assertThat(representation, is("My car has an engine size of 20000 cubic centimetres"));
     }
@@ -38,14 +39,15 @@ public class VolumeAttributeRendererTest {
     @Test
     public void formatsCcInShortFormat() {
         String representation = renderStringTemplate("My car has an engine size of $engineSize;format=\"cc-short\"$",
-                                                        attribute("engineSize", new Volume(20, LITRES)));
+                                                     with().attribute("engineSize", new Volume(20, LITRES)));
 
         assertThat(representation, is("My car has an engine size of 20000cc"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void failsIfFormatNotRecognised() {
-        renderStringTemplate("My car has an engine size of $engineSize;format=\"foobar\"$", attribute("engineSize", new Volume(20, LITRES)));
+        renderStringTemplate("My car has an engine size of $engineSize;format=\"foobar\"$",
+                             with().attribute("engineSize", new Volume(20, LITRES)));
     }
 
 }

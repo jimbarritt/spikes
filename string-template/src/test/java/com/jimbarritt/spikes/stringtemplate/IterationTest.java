7dce8d58e19dc593342af46af1dc7c62ffe5a1dd
Jim Barritt <jim@planet-ix.com>
2010-07-23 11:49:26 +0100

    Added demonstration of using trunc and last for formatting a list

--- string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/IterationTest.java
+++ string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/IterationTest.java
@@ -2,6 +2,7 @@ package com.jimbarritt.spikes.stringtemplate;
 
 import com.jimbarritt.spikes.stringtemplate.io.*;
 import org.antlr.stringtemplate.*;
+import org.apache.log4j.*;
 import org.junit.*;
 
 import java.util.*;
@@ -9,13 +10,33 @@ import java.util.*;
 import static java.util.Arrays.asList;
 
 public class IterationTest {
+    private static final Logger log = Logger.getLogger(IterationTest.class);
+    private StringTemplateGroup group;
+    private List<String> listOfThings;
+
+    @Before
+    public void onceBeforeEachTest() {
+        group = new StringTemplateLoader().loadGroupFromClasspath("st/formatting/list.stg");
+        listOfThings = asList("foo", "bar", "choco", null, "monkey");
+    }
 
     @Test
-    public void differentSeparatorAtEndOfList() {
-        List<String> listOfThings = asList("foo", "bar", "choco", "monkey");
+    public void rendersASimpleList() {
+        StringTemplate template = group.getInstanceOf("simpleList");
+        template.setAttribute("list", listOfThings);
 
-        
+        String representation = new StringTemplateRenderer().render(template);
 
+        log.info("Representation\n" + representation);
+    }
 
+    @Test
+    public void rendersAListWithASeparator() {
+        StringTemplate template = group.getInstanceOf("listWithSeparator");
+        template.setAttribute("list", listOfThings);
+
+        String representation = new StringTemplateRenderer().render(template);
+
+        log.info("Representation\n" + representation);
     }
 }

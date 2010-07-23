695ba859ede30d8196ce50088033c50fa537c672
Jim Barritt <jim@planet-ix.com>
2010-07-23 08:59:37 +0100

    Tidyied up test

--- string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/CustomFormatTest.java
+++ string-template/src/test/java/com/jimbarritt/spikes/stringtemplate/CustomFormatTest.java
@@ -22,19 +22,7 @@ public class CustomFormatTest {
         String representation = renderStringTemplate("My car has an engine size of $engineSize;format=\"cc\"$",
                                                      new Volume(20, Volume.UnitOfMeasure.LITRES));
 
-        System.out.println("[" + representation + "]");
-
-        assertThat(representation.equalsIgnoreCase("My car has an engine size of 20000 cubic centimetres"), is(true));
-        assertEquals("My car has an engine size of 20000 cubic centimetres", representation);
         assertThat(representation, is("My car has an engine size of 20000 cubic centimetres"));
-        assertThat(representation.length(), is("My car has an engine size of 20000 cubic centimeters".length()));
-        String expected = "My car has an engine size of 20000 cubic centimeters";
-        for (int iChar=0; iChar<representation.length();++iChar) {
-            if (representation.charAt(iChar) != expected.charAt(iChar)) {
-                System.out.println("Did not match char [" + iChar + "] " + representation.charAt(iChar) + " != " + expected.charAt(iChar));
-            }
-        }
-        assertThat(representation, is(expected));
     }
 
     private static String renderStringTemplate(String template, Volume volume) {

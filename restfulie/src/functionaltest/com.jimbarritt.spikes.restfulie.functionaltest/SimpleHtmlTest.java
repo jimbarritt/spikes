9e31539845ae03aff58ac0fafbdc13c6144b5f65
Jim Barritt <jim@planet-ix.com>
2010-08-21 21:54:46 +0100

    Changed LocationResource to simple HtmlResource for demo

--- restfulie/src/functionaltest/com.jimbarritt.spikes.restfulie.functionaltest/SimpleHtmlTest.java
+++ restfulie/src/functionaltest/com.jimbarritt.spikes.restfulie.functionaltest/SimpleHtmlTest.java
@@ -7,15 +7,15 @@ import org.junit.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
-public class LocationTest {
+public class SimpleHtmlTest {
 
-    private static final Logger log = Logger.getLogger(LocationTest.class);
+    private static final Logger log = Logger.getLogger(SimpleHtmlTest.class);
 
     @Test
     public void canGetALocation() throws Exception {
         SimpleHttpClient httpClient = new SimpleHttpClient();
 
-        String representation = httpClient.getFrom("http://localhost:8080/restfulie-spike/locations/34523");
+        String representation = httpClient.getFrom("http://localhost:8080/restfulie-spike/simplehtml/34523");
         
         assertThat(representation, containsString("34523"));
     }
--- restfulie/src/main/java/com/jimbarritt/spikes/restfulie/server/resources/SimpleHtmlResource.java
+++ restfulie/src/main/java/com/jimbarritt/spikes/restfulie/server/resources/SimpleHtmlResource.java
@@ -6,16 +6,16 @@ import br.com.caelum.vraptor.view.*;
 import static java.lang.String.format;
 
 @Resource
-public class LocationResource {
+public class SimpleHtmlResource {
 
     private final Result result;
 
-    public LocationResource(Result result) {
+    public SimpleHtmlResource(Result result) {
         this.result = result;
     }
 
     @Get
-    @Path("/locations/{id}")
+    @Path("/simplehtml/{id}")
     public void location(int id) {
         result.use(DefaultHttpResult.class).body(format("<html><h1>Oh yeah baby, VRaptor Rockin! id is %d</h1></html>", id));
     }

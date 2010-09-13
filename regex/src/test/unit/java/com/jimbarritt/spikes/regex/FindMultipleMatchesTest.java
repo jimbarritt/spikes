68450dac86e82c9c346c04522b97d184e4f7dd9f
Jim Barritt <jim@planet-ix.com>
2010-09-13 21:31:02 +0100

    Working out how to pick out parenthesese

--- regex/src/test/unit/java/com/jimbarritt/spikes/regex/FindMultipleMatchesTest.java
+++ regex/src/test/unit/java/com/jimbarritt/spikes/regex/FindMultipleMatchesTest.java
@@ -5,13 +5,15 @@ import org.apache.log4j.*;
 
 import java.util.regex.*;
 
+import static java.util.regex.Pattern.compile;
+
 public class FindMultipleMatchesTest {
 
     private static final Logger log = Logger.getLogger(FindMultipleMatchesTest.class);
 
     @Test
     public void findsMultipleGroups() {
-        Pattern pattern = Pattern.compile("\\w* ");
+        Pattern pattern = compile("\\w* ");
         
         Matcher matcher = pattern.matcher("this is a sentance");
         log.info("GroupCount: " + matcher.groupCount());
@@ -22,10 +24,21 @@ public class FindMultipleMatchesTest {
 
     @Test
     public void findStructuredGroups() {
-        Pattern pattern = Pattern.compile("\\{\\w*\\}");
+        Pattern pattern = compile("\\{\\w*\\}");
         Matcher matcher = pattern.matcher("/{parameter1}/some/path/{parameter2}/end");
         while(matcher.find()) {
             log.info("Found parameter: [" + matcher.group().replaceAll("\\{|\\}", "") + "]");
+        }
+        String output = matcher.replaceAll("(.*)");
+        log.info("Replaced output: [" + output + "]");
+    }
+
+    @Test
+    public void findStructuredGroupsWithParenthesis() {
+        Pattern pattern = compile("\\([^\\)]*\\)");
+        Matcher matcher = pattern.matcher("Some sentance (parameter 1d) /some/path/ (parameter 2 ) /end");
+        while(matcher.find()) {
+            log.info("Found parameter: [" + matcher.group().replaceAll("\\(|\\)", "") + "]");
         }
         String output = matcher.replaceAll("(.*)");
         log.info("Replaced output: [" + output + "]");

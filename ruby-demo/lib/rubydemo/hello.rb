9f208c2a050c13eb122e72db71ff4e110372659d
Jim Barritt <jim@planet-ix.com>
2010-11-08 00:28:12 +0000

    Playing with autotest

--- ruby-demo/lib/rubydemo/hello.rb
+++ ruby-demo/lib/rubydemo/hello.rb
@@ -1,12 +1,22 @@
 module RubyDemo
 
 class Hello
+
+  def initialize
+    @count = 0
+  end
+
   def speak    
     return true
   end
 
   def speakTo name
+    @count = @count += 1
     return "Hello #{name}"
+  end
+
+  def numberOfTimesCalled
+    return @count
   end
   
 end
--- ruby-demo/test/rubydemo/test_hello.rb
+++ ruby-demo/test/rubydemo/test_hello.rb
@@ -3,21 +3,33 @@ require "rubydemo/hello"
 
 class TC_HelloTest < Test::Unit::TestCase
 
-  def test_speak
-    hello = RubyDemo::Hello.new
-    assert hello.speak, "Speak should be true"
+  def setup
+    @hello = RubyDemo::Hello.new
   end
 
-  def test_says_hello_to_jim
-    hello = RubyDemo::Hello.new
-    result = hello.speakTo "Jim"
+  def test_speak
+    assert @hello.speak, "Speak should be true"
+  end
+
+  def test_says_hello_to_jim    
+    result = @hello.speakTo "Jim"
     assert_equal "Hello Jim", result
   end
 
   def test_says_hello_to_john
-    hello = RubyDemo::Hello.new
-    result = hello.speakTo "John"
+    result = @hello.speakTo "John"
     assert_equal "Hello John", result
+  end
+
+  def test_remembers_how_many_times_it_was_called
+    @hello.speakTo "Bill"
+    @hello.speakTo "Fred"
+    @hello.speakTo "Phil"
+
+    count = @hello.numberOfTimesCalled
+
+    assert_equal 3,  count
+
   end
 end
 

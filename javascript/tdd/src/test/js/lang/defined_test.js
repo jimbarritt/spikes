76f953d3e33929dd81a839a852016414ddfd19a3
Jim Barritt <jim@planet-ix.com>
2010-11-13 22:51:50 +0000

    Ok got it all working

--- javascript/tdd/src/test/js/lang/defined_test.js
+++ javascript/tdd/src/test/js/lang/defined_test.js
@@ -4,7 +4,7 @@ TestCase("DefinedTest", {
     tearDown : function(){},
 
     test_knows_if_an_object_is_not_defined : function() {
-        var somethingIDefined = "foo";
+        var somethingIDefined = "foo ".trim();
 
         var isNotDefined = lang.isNotDefined(somethingIDefined);
 

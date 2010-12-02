d4105ebb01348d2c1b86c8c46017119005379fad
jimbarritt <jim@planet-ix.com>
2010-12-02 19:45:50 +0000

    Adding clojure intro

---
+++ clojure-intro/src/hello_world.clj
@@ -0,0 +1,1 @@
+(def jim "Hello)"
---
+++ clojure-intro/test/clojure_intro/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-intro.test.core
+  (:use [clojure-intro.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

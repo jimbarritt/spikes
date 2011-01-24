7d7a914c7e5d0ea519c01d86d2393a67a95864f8
jimbarritt <jim@planet-ix.com>
2011-01-24 21:50:03 +0530

    Adding Twist Spike

---
+++ twist/twist-tests/functional-tests/src/test/twist/java/org/jimbarritt/spikes/twist/workflow/GoogleSearch.java
@@ -0,0 +1,11 @@
+package org.jimbarritt.spikes.twist.workflow;
+
+import static junit.framework.Assert.assertTrue;
+
+public class GoogleSearch {
+
+	public void assertThatTrueIs(boolean input) throws Exception {
+		assertTrue(input + " Should be true", input);
+	}
+
+}

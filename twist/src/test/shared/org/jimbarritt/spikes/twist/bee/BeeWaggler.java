3ee574c4702fbd9a4e0f65b62515254c38651c59
jimbarritt <jim@planet-ix.com>
2011-01-25 14:27:58 +0530

    Adding referenced librarys

--- twist/src/test/shared/org/jimbarritt/spikes/twist/bee/BeeWaggler.java
+++ twist/src/test/shared/org/jimbarritt/spikes/twist/bee/BeeWaggler.java
@@ -1,10 +1,14 @@
 package org.jimbarritt.spikes.twist.bee;
 
+import org.apache.log4j.*;
+
 import java.util.*;
 
 public class BeeWaggler {
+    private static final Logger log = Logger.getLogger(BeeWaggler.class);
 
     public List<Bee> waggleSomeBees() {
+        log.error("Hello Im a gooona waggle me some bees dude!");
         return Arrays.asList(new Bee(), new Bee(), new Bee());
     }
 }

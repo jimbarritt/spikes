111c30884a87b5583f5ac4cef51b9ab926b8e7ea
Jim Barritt <jim@planet-ix.com>
2011-03-06 15:55:36 +0530

    Got sinatra running

--- ruby-web/sinatra/run_server.sh
+++ ruby-web/sinatra/run_server.sh
@@ -1,6 +1,13 @@
-#!usr/bin/bash
+#!/bin/bash
 
 SINATRA_GEM=$(gem list --local | grep sinatra)
 
-echo "Sinatra Gem : $SINATRA_GEM"
+if [ "$SINATRA_GEM" != "" ]; then
+	echo "Sinatra Gem : [$SINATRA_GEM]"
+else	
+	echo "Going to install sinatra gem..."	
+	gem install sinatra
+fi
 
+echo "Running hi.rb now"
+ruby -rubygems hi.rb
